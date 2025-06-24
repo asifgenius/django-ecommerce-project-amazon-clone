eeb6b301a42f85876ce83a8d0b373caf885cb72b
Asif Islam <engrasifislam@gmail.com>
2025-06-24 20:01:49 +0600

    Add django_filters package to project settings

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DjangoEcommerceApp',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'DjangoEcommerce.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
